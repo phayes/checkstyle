ae9f37d00cd40bd2645430a0497cf5a4419719a8
phayes <patrick.d.hayes@gmail.com>
2017-09-01 18:37:07 -0700

    Updating godoc

--- godoc.go
+++ godoc.go
@@ -5,28 +5,28 @@ Checkstyle XML files are a standard file format for reporting errors in source c
 
 Example usage:
   // Print XML into human readable format
-	checkSyle, err := checkstyle.ReadFile("checkstyle_report.xml")
-	  if err != nil {
-	  log.Fatal(err)
-	}
-	for _, file := range checkStyle.File {
-	  fmt.Println(File.Name)
+  checkSyle, err := checkstyle.ReadFile("checkstyle_report.xml")
+    if err != nil {
+    log.Fatal(err)
+  }
+  for _, file := range checkStyle.File {
+    fmt.Println(File.Name)
     for _, codingError := range file.Error {
       fmt.Println("\t", codingError.Line, codingError.Message)
     }
-	}
+  }
 
   // Create a new XML file from scratch
-	check := checkstyle.New()
+  check := checkstyle.New()
 
   // Ensure that a file has been added
-	file := check.EnsureFile("/path/to/file")
+  file := check.EnsureFile("/path/to/file")
 
   // Create an error on line 10
-	codingError := checkstyle.NewError(10, "format", "line must end with a full stop")
+  codingError := checkstyle.NewError(10, "format", "line must end with a full stop")
 
   // Add the error to the file
-	file.AddError(codingError)
+  file.AddError(codingError)
 
   // Output XML
   fmt.Print(check)
